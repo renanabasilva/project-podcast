f909407c14c43d764e3935c80d5c29a760b9ef21
Renan Aba <124288351+renanabasilva@users.noreply.github.com>
2023-05-12 16:24:50 +0100

    Finalização do projeto
    Finalização do projeto: Inclusão e ajuste das imagens, bug fix, remoção de configurações não utilizadas, adição e organização de comentários

--- javascript/script.js
+++ javascript/script.js
@@ -1,33 +1,4 @@
-// function sizeChange() {
-//     if (window.innerWidth >=768) {
-//         itens.style.display = 'block'
-//     } else {
-//         itens.style.display = 'none'
-//     }
-// }
-
-// function clickMenu() {
-//     if (menu.style.visibility == 'visible') {
-//         menu.style.visibility = 'hidden'
-//         closemenu.style.visibility = 'hidden'
-//     } else {
-//         menu.style.visibility = 'visible'
-//         closemenu.style.visibility = 'visible'
-//     }
-// }
-
-// const menuBtn = document.getElementById('burger');
-// const menu = document.getElementById('menu');
-
-// menuBtn.addEventListener('click', function() {
-//   menu.classList.toggle('open');
-// });
-
 const menu = document.getElementById('menu')
 function clickMenu() {
     menu.classList.toggle('open');
 }
-
-// function exitMenu() {
-//     menu.style.top = '-100%'
-// }
